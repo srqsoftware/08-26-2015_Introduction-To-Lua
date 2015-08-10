c11ac883187ebe6f11b4387ffe41631f55997441
Stratus3D <admin@stratus3d.com>
2015-08-09 21:41:13 -0400

    Add comments and strings code sample.

---
+++ code_samples/comments_and_strings.lua
@@ -0,0 +1,10 @@
+-- comments start with two dashes
+--[[
+    adding two square brackets
+    makes the comment multi-line
+--]]
+
+string = 'some text in single quotes'
+
+-- double quotes are treated the same as single quotes
+another_sting = "some text in double quotes"
