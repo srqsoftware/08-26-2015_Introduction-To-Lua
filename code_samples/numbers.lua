33a537008f41ede828b91d74637e137886628329
Stratus3D <admin@stratus3d.com>
2015-08-13 06:40:26 -0400

    Complete numbers code sample.

--- code_samples/numbers.lua
+++ code_samples/numbers.lua
@@ -1,9 +1,13 @@
--- all numbers are doubles (double precision floating point values)
--- Lua preserves exact integer values (52 bits are used to store the integer value)
-num = 42 
+-- all numbers are double precision floating
+-- point values (and return IEEE 754 floats)
+-- Lua preserves exact integer values (52
+-- bits are used to store the integer value)
+num = 42
 
-num2 = num / 9 -- 4.6666666666667
+-- arithmetic expressions that can return integer values will
 num3 = num / 7 -- 6.0
-num4 = num + 0.5
-num4 -- 42.5
-(6 == num3) -- true (6 == 6.0)
+equal = (6 == num3) -- true -- (6 == 6.0)
+
+-- some can't
+num2 = num / 9 -- 4.6666666666667
+num4 = num + 0.5 -- 42.5
