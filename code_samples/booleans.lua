ac5fa97bf9bb64b98ed8c258dc52738673cfaea4
Stratus3D <admin@stratus3d.com>
2015-08-15 17:28:56 -0400

    Add nil code samples.

--- code_samples/booleans.lua
+++ code_samples/booleans.lua
@@ -8,5 +8,6 @@ if not oppositeValue then print('it was false') end
   -- it was false
 
 -- `not` can be used to coerce values of other types to booleans
+-- all values except nil and false are considered truthy
 print(not 'a') -- false
 print(not not 'a') -- true
---
+++ code_samples/nil.lua
@@ -0,0 +1,9 @@
+-- nil - Lua's undefined or null value
+-- when a key is missing in a table nil is returned
+table = {}
+print(table.nonExistent) -- nil
+
+-- setting a variable to nil allows the value of
+-- the variable to be garbage collected
+largeValue = nil
+-- largeValue will now be removed from memory
