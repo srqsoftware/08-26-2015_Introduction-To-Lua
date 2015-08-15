ecd67d94e113700d1b5e8137b54c8619a1272b42
Stratus3D <admin@stratus3d.com>
2015-08-15 16:31:00 -0400

    Add more details to the numbers code sample.

--- code_samples/numbers.lua
+++ code_samples/numbers.lua
@@ -1,5 +1,5 @@
 -- all numbers are double precision floating
--- point values (and return IEEE 754 floats)
+-- point values (IEEE 754 floats)
 -- Lua preserves exact integer values (52
 -- bits are used to store the integer value)
 num = 42
@@ -11,3 +11,5 @@ equal = (6 == num3) -- true -- (6 == 6.0)
 -- some can't
 num2 = num / 9 -- 4.6666666666667
 num4 = num + 0.5 -- 42.5
+
+-- http://lua-users.org/wiki/NumbersTutorial
