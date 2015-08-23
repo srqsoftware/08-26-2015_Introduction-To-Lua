fba0b58224ba8ad515694857a4d158e725a9fef4
Stratus3D <admin@stratus3d.com>
2015-08-22 22:42:32 -0400

    Complete the metatables code sample.

--- code_samples/metatables.lua
+++ code_samples/metatables.lua
@@ -1,10 +1,15 @@
 -- metatables
 -- metatables are similiar to JavaScript prototypes and provide
 -- a sort of operator overloading
-defaults_metatable = {favorite_language = 'Lua'}
-defaults_metatable.__index = defaults_metatable
-table = {}
+user_defaults = {favorite_language = 'Lua'}
+-- metatables can contain various attributes that are read by Lua when
+-- performing certainly operations. For example,
+-- __index is a metatable attribute Lua checks when a key is missing in a table
+user_defaults.__index = user_defaults
+user = {}
 
-setmetatable(table, defaults_metatable)
-print(getmetatable(table)) --> table: 0x15ebe60
-print(table.favorite_language) --> 'Lua'
+-- if we set user_defaults as the metatable lua will check the table assigned
+-- to the metatables `__index` attribute
+setmetatable(user, user_defaults)
+print(getmetatable(user)) --> table: 0x15ebe60
+print(user.favorite_language) --> 'Lua'
